5e06b1333f1ce539f5e092d7b161ddd8b9b567d2
Tu Quynh <aqv9882@nyu.edu>
2022-11-28 21:19:44 +0800

    Initial commit

---
+++ blank_p5_template/script.js
@@ -0,0 +1,13 @@
+console.log("my script is loaded successfully")
+
+
+function setup(){
+    let cnv = createCanvas(200,200);
+    cnv.parent("canvasContainer");
+
+
+    background(71, 174, 163);
+}
+
+function draw(){
+}
